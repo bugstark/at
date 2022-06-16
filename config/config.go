f6847679eb1450ae149d5580e1336edc2c50d5ef
bugstark <admin@an2.cn>
2022-06-16 15:31:12 +0800

    add config

---
+++ config/config.go
@@ -0,0 +1,42 @@
+package config
+
+import (
+	"flag"
+	"log"
+	"os"
+
+	"github.com/spf13/viper"
+)
+
+var c *viper.Viper
+
+func init() {
+	var config string
+	flag.StringVar(&config, "c", "", "choose config file.")
+	flag.Parse()
+	if config == "" { // 优先级: 命令行 > 环境变量 > 默认值
+		if configEnv := os.Getenv("TiTan_CONFIG"); configEnv == "" {
+			config = "config.yaml"
+			log.Printf("您正在使用config的默认值\n")
+		} else {
+			config = configEnv
+			log.Printf("您正在使用GVA_CONFIG环境变量,config的路径为%v\n", config)
+		}
+	} else {
+		log.Printf("您正在使用命令行的-c参数传递的值,config的路径为%v\n", config)
+	}
+
+	v := viper.New()
+	v.SetConfigFile(config)
+	v.SetConfigType("yaml")
+	err := v.ReadInConfig()
+	if err != nil {
+		log.Fatalf("Fatal error config file: %s \n", err)
+	}
+	v.WatchConfig()
+	c = v
+}
+
+func Get() *viper.Viper {
+	return c
+}
