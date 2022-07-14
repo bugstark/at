2d5aa7b76b548f98dfaf000733d30d8c11ae4363
bugstark <admin@an2.cn>
2022-07-14 14:21:31 +0800

    engine

---
+++ engine/gin.go
@@ -0,0 +1,23 @@
+package engine
+
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+)
+
+func NewEngine(debug bool) *gin.Engine {
+	if debug {
+		gin.SetMode(gin.DebugMode)
+	}
+	var app = gin.New()
+	app.MaxMultipartMemory = 8 << 20
+	app.Use(gin.Recovery())
+	app.NoRoute(func(c *gin.Context) {
+		c.AbortWithStatusJSON(404, gin.H{
+			"msg":  fmt.Sprintf("not found '%s:%s'", c.Request.Method, c.Request.URL.Path),
+			"data": nil,
+		})
+	})
+	return app
+}
