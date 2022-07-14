f1291f3093585214b51b61b2208cd19e4c8a2101
bugstark <admin@an2.cn>
2022-07-14 19:23:59 +0800

    comm query

--- datas/init.go
+++ datas/init.go
@@ -19,3 +19,12 @@ func InitDB(drive, dsn string, debug bool) {
 func InitRedis(host, pass string, port, db int) {
 	Redis = initredis(host, pass, port, db)
 }
+
+// Query 查询参数
+type Query struct {
+	Fields string `gorm:"-" form:"fields" json:"-"`
+	Sort   string `gorm:"-" form:"sort" json:"-"`
+	Order  string `gorm:"-" form:"order" json:"-"`
+	Size   int    `gorm:"-" form:"size" json:"-"`
+	Page   int    `gorm:"-" form:"page" json:"-"`
+}
