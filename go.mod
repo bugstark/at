bb424c0fb82eb556c3d866df201e7e493df1ed6c
bugstark <admin@an2.cn>
2022-07-15 10:22:27 +0800

    移动短信接口

--- go.mod
+++ go.mod
@@ -44,6 +44,7 @@ require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/gin-gonic/gin v1.8.1
 	github.com/go-redis/redis v6.15.9+incompatible
+	github.com/imroc/req v0.3.2
 	github.com/mohae/deepcopy v0.0.0-20170929034955-c48cc78d4826 // indirect
 	github.com/richardlehane/mscfb v1.0.4 // indirect
 	github.com/richardlehane/msoleps v1.0.1 // indirect
---
+++ service/MAS.go
@@ -0,0 +1,66 @@
+package service
+
+import (
+	"crypto/md5"
+	"encoding/base64"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"log"
+
+	"github.com/imroc/req"
+)
+
+type MAS struct {
+	EcName    string
+	Apid      string
+	SecretKey string
+	Sign      string
+	Apiurl    string
+}
+
+func NewMAS(ecname, apid, secretkey, sign, apiurl string) *MAS {
+	return &MAS{
+		EcName:    ecname,
+		Apid:      apid,
+		SecretKey: secretkey,
+		Sign:      sign,
+		Apiurl:    apiurl,
+	}
+}
+
+// SendSMS 多个电话号码,英文逗号分隔
+func (m *MAS) SendSMS(phone, content string) error {
+	data := map[string]string{}
+	data["ecName"] = m.EcName
+	data["apId"] = m.Apid
+	data["mobiles"] = phone
+	data["content"] = content
+	data["sign"] = m.Sign
+	data["addSerial"] = ""
+	data["mac"] = fmt.Sprintf("%x", md5.Sum([]byte(m.EcName+m.Apid+m.SecretKey+phone+content+m.Sign)))
+	jsondata, err := json.Marshal(data)
+	if err != nil {
+		return err
+	}
+	base64stringjson := base64.StdEncoding.EncodeToString(jsondata)
+	res, err := req.Post(m.Apiurl, base64stringjson)
+	if err != nil {
+		return err
+	}
+	type RESP struct {
+		MsgGroup string `json:"msgGroup"`
+		Rspcod   string `json:"rspcod"`
+		Success  bool   `json:"success"`
+	}
+	var resp RESP
+	err = res.ToJSON(&resp)
+	if err != nil {
+		return err
+	}
+	if resp.Success {
+		return nil
+	}
+	log.Println(resp.Rspcod)
+	return errors.New(resp.Rspcod)
+}
