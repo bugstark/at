47462e9a74352f8c74315e3baad569a2cf838f9b
bugstark <admin@an2.cn>
2022-07-16 23:07:49 +0800

    redis

--- datas/redis.go
+++ datas/redis.go
@@ -7,18 +7,18 @@ import (
 	"github.com/go-redis/redis"
 )
 
-func initredis(host, pass string, port, db int) *redis.Client {
-	// 设置redis客户端
+var Rdb *redis.Client
+
+func Initredis(host, pass string, port, db int) {
 	log.Printf("连接redis [%s:%d]", host, port)
-	rdb := redis.NewClient(&redis.Options{
+	Rdb = redis.NewClient(&redis.Options{
 		Addr:     fmt.Sprintf("%s:%d", host, port),
 		Password: pass, // no password set
 		DB:       db,   // use default DB
 	})
 	// 测试连接
-	_, e := rdb.Ping().Result()
+	_, e := Rdb.Ping().Result()
 	if e != nil {
 		log.Fatalf("连接redis失败! [%s:%d]", host, port)
 	}
-	return rdb
 }
--- go.mod
+++ go.mod
@@ -78,6 +78,5 @@ require (
 
 require (
 	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0 // indirect
-	github.com/gomodule/redigo v1.8.9
 	golang.org/x/image v0.0.0-20211028202545-6944b10bf410 // indirect
 )
