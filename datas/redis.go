b6df4341aaaf959284ccfb1e94d44706b4ee042f
bugstark <admin@an2.cn>
2022-07-16 23:10:23 +0800

    redis

--- datas/redis.go
+++ datas/redis.go
@@ -7,18 +7,17 @@ import (
 	"github.com/go-redis/redis"
 )
 
-var Rdb *redis.Client
-
-func Initredis(host, pass string, port, db int) {
+func initredis(host, pass string, port, db int) *redis.Client {
 	log.Printf("连接redis [%s:%d]", host, port)
-	Rdb = redis.NewClient(&redis.Options{
+	rdb := redis.NewClient(&redis.Options{
 		Addr:     fmt.Sprintf("%s:%d", host, port),
 		Password: pass, // no password set
 		DB:       db,   // use default DB
 	})
 	// 测试连接
-	_, e := Rdb.Ping().Result()
+	_, e := rdb.Ping().Result()
 	if e != nil {
 		log.Fatalf("连接redis失败! [%s:%d]", host, port)
 	}
+	return rdb
 }
