e341134554cfaf95c376128895921d51684483ec
bugstark <admin@an2.cn>
2022-07-20 13:14:47 +0800

    captcha 4 to 6

--- captcha/captcha.go
+++ captcha/captcha.go
@@ -16,7 +16,7 @@ import "github.com/mojocn/base64Captcha"
 // }
 
 var store = base64Captcha.DefaultMemStore
-var driver base64Captcha.Driver = base64Captcha.NewDriverDigit(80, 240, 4, 0.7, 80)
+var driver base64Captcha.Driver = base64Captcha.NewDriverDigit(80, 240, 6, 0.7, 80)
 
 // 生成验证码
 func Generate() (id, b64s string, err error) {
